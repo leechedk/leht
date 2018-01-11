1bd92413f50f79009cab680fd3bb0702c1c5bbd7
karleerikkokk1 <karl-eerikkokk@khk.ee>
2018-01-11 10:35:32 +0200

    updated theme `Zerif Lite` version 1.8.5.35

--- wp-content/themes/zerif-lite/header.php
+++ wp-content/themes/zerif-lite/header.php
@@ -14,7 +14,6 @@
 <?php zerif_top_head_trigger(); ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="keywords" content="hello, darkness, stuff, doom, onjunii">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
